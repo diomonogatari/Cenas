64b56cb2bc610a52c80f100dc08da67e8f0efed8
Diogo Rosa Carvalho <diodude.lol@gmail.com>
2018-12-10 21:05:32 +0000

    Init

---
+++ Cenas/Cenas/Character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cenas
+{
+    class Character
+    {
+        /*Character Properties*/
+        int Health { get; set; }
+        string Name { get; set; }
+        int Strength { get; set; }
+        int Perception { get; set; }
+        int Endurance { get; set; }
+        int Charisma { get; set; }
+        int Intelligence { get; set; }
+        int Agility { get; set; }
+        int Luck { get; set; }
+
+
+        public Character()
+        {
+
+        }
+    }
+}
---
+++ Cenas/Character.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Character
+{
+    /*Character Properties*/
+    int hp { get; set; }
+    string name { get; set; }
+    int hp { get; set; }
+    int hp { get; set; }
+    int hp { get; set; }
+    int hp { get; set; }
+    int hp { get; set; }
+    int hp { get; set; }
+    int hp { get; set; }
+    int hp { get; set; }
+
+
+    public Character()
+	{
+
+	}
+}
