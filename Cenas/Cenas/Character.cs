4cb843a04178509919e24ab423e9cf97bce1dbfe
Diogo Rosa Carvalho <diodude.lol@gmail.com>
2018-12-10 21:06:39 +0000

    .

--- Cenas/Cenas/Character.cs
+++ Cenas/Cenas/Character.cs
@@ -9,20 +9,33 @@ namespace Cenas
     class Character
     {
         /*Character Properties*/
-        int Health { get; set; }
-        string Name { get; set; }
-        int Strength { get; set; }
-        int Perception { get; set; }
-        int Endurance { get; set; }
-        int Charisma { get; set; }
-        int Intelligence { get; set; }
-        int Agility { get; set; }
-        int Luck { get; set; }
+        private int Health { get; set; }
+        private string Name { get; set; }
+        private int Strength { get; set; }
+        private int Perception { get; set; }
+        private int Endurance { get; set; }
+        private int Charisma { get; set; }
+        private int Intelligence { get; set; }
+        private int Agility { get; set; }
+        private int Luck { get; set; }
 
 
         public Character()
         {
 
+        }
+
+        public Character(int health, string name, int strength, int perception, int endurance, int charisma, int intelligence, int agility, int luck)
+        {
+            Health = health;
+            Name = name;
+            Strength = strength;
+            Perception = perception;
+            Endurance = endurance;
+            Charisma = charisma;
+            Intelligence = intelligence;
+            Agility = agility;
+            Luck = luck;
         }
     }
 }
